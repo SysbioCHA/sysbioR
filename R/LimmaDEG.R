c813264769bef559aa927c019b5eaaf9b996b61c
hoon1866 <hoon1866@naver.com>
2022-08-01 19:45:50 +0900

    limmadeg

--- R/LimmaDEG.R
+++ R/LimmaDEG.R
@@ -2,12 +2,43 @@
 #'
 #' This function performs DEG analysis using
 #'
-#' @param mat matrix
-#' @param anno annotation dataframe
+#' @param motherMatrix row name should be probe or gene and col name should be sample names.
+#' @param case pattern of case samples
+#' @param control pattern of control samples
 #' @return res DEG results.
 #' @examples
+#' mothermatrix colnames: treat_A, treat_B, treat_C, no_treat_A, no_treat_B, no_treat_C
+#' LimmaDEG(mat, "treat", "no_treat")
 #' @export
-LimmaDEG<-function(mat, anno){
-  res = c(1,2,3)
-  return(res)
+LimmaDEG <- function(motherMatrix  , case, control){
+  x = motherMatrix
+  y = case
+  z = control
+  HT <- x %>% as.data.frame() %>%  dplyr::select(contains(y)|contains(z))
+  rownames(HT) = rownames(motherMatrix)
+
+  cse <- c()
+  for(k in 1:length(grep(y, colnames(x)))){
+    cse[k] <- y
+  }
+  ctrl <- c()
+  for(k in 1:length(grep(z, colnames(x)))){
+    ctrl[k] <- z
+  }
+
+  HT_group <- c(paste("A",cse,sep = "_"), paste("B",ctrl,sep = "_")) %>% as.factor()
+  design <- model.matrix(~-1+ HT_group)
+
+  HT_group <- factor(x= c(cse, ctrl),levels = c(y,z))
+  colnames(design) <- levels(HT_group)
+
+  fit <- lmFit(HT, design)
+  constrast.matrix<-makeContrasts(  sprintf("%s - %s", y, z), levels=HT_group)
+  fit.cont<-contrasts.fit(fit,constrast.matrix)
+  efit<- eBayes(fit.cont)
+  result<-topTable(efit, n=nrow(efit))
+
+  DEGs <- result
+  DEGs[, 'id'] <- rownames(DEGs)
+  return(DEGs)
 }
